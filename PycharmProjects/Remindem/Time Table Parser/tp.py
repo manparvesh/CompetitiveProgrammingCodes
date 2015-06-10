72b4f0b156698862306cc2fd2496b0220f6fdb97
Man Parvesh <manparveshsinghrandhawa@gmail.com>
2015-06-10 21:33:15 +0530

    Removed Remindem project

--- PycharmProjects/Remindem/Time Table Parser/tp.py
+++
@@ -1,8 +0,0 @@
-from __future__ import division
-if __name__ == '__main__':
-    n = int(raw_input())
-    for _ in range(n):
-        s=raw_input().split()
-        a=int(s[0])
-        b=int(s[1])
-        print a//b, a%b
